7e61370d3056927e39a0c695be03d664bc5890bb
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2025-01-21 04:24:08 +0800

    add game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
-import matcha from "./assets/matcha.jpg";
 
 function App() {
   const [highScore, setHighScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
   const [isPlaying, setIsPlaying] = useState(true);
-  const [imgList, setImgList] = useState([
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-    matcha,
-  ]);
+  const [selected, setSelected] = useState([]);
+  const [imgList, setImgList] = useState([]);
+  const url = "https://api.thecatapi.com/v1/images/search?limit=10";
 
-  console.log(window.innerWidth);
+
+  function dealCards(selectedCards, apiCards) {
+    if (selectedCards.length === 0) return apiCards;
+
+    const cards = apiCards;
+    const randomSelection = Math.floor(Math.random() * selectedCards.length);
+    const replaceIndex = Math.floor(Math.random() * 8);
+    cards[replaceIndex] = selectedCards[randomSelection];
+    return cards;
+  }
+
+  function selectCard(e) {
+    const link = e.target.src;
+
+    if (selected.includes(link)) {
+      setHighScore(currentScore);
+      setCurrentScore(0);
+      setSelected([]);
+      alert("Game Over!");
+    } else {
+      const newSelected = [...selected, link];
+      setSelected(newSelected);
+      setCurrentScore(currentScore + 1);
+    }
+  }
+
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) {
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const urlList = data.map((d) => d.url);
+          urlList.length = 8;
+          const newCards = dealCards(selected, urlList);
+          setImgList(newCards);
+        });
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, [selected]);
 
   return (
     <>
@@ -35,7 +68,13 @@ function App() {
       </div>
 
       <div className="container card-area">
-        {imgList.map(img => <Card imgSrc={img} key={crypto.randomUUID()}></Card>)}
+        {imgList.map((img) => (
+          <Card
+            imgSrc={img}
+            handleClick={selectCard}
+            key={crypto.randomUUID()}
+          ></Card>
+        ))}
       </div>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import "../styles/card.css";
 
-export default function Card({ imgSrc }) {
+export default function Card({ imgSrc, handleClick }) {
   return (
     <div className="card">
-      <img src={imgSrc}></img>
+      <img src={imgSrc} onClick={handleClick}></img>
     </div>
   );
 }
